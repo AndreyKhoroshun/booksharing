83f0cd9e698c629f248cd81b3dea8673ab5c44a9
AndreyKhoroshiy <Khoroshiy256@gmail.com>
2021-02-14 00:06:51 +0200

    add model Book

--- app/books/models.py
+++ app/books/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    author = models.CharField(max_length=128)
+    title = models.CharField(max_length=128)
+    publish_year = models.PositiveSmallIntegerField()
+    review = models.CharField(max_length=512)
+    condition = models.PositiveSmallIntegerField()
