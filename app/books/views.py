25eba3069beeed010296a198e9f73f2de7b5bbf0
AndreyKhoroshiy <Khoroshiy256@gmail.com>
2021-02-14 01:32:03 +0200

    add function book_list

--- app/books/views.py
+++ app/books/views.py
@@ -1,18 +1,9 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-
-import string
-import random
+from books.models import Book
 
 
-def gennerate_password(lenght: int = 10) -> str:
-    password = ''
-    for _ in range(lenght):
-        password += random.choice(string.ascii_letters)
-    return password
-
-
-def hello_world(request):
-    length = int(request.GET.get('length') or 10)
-    password = gennerate_password(length)
-    return HttpResponse(password)
+def book_list(request):
+    response_content = ''
+    for book in Book.objects.all():
+        response_content += f'ID: {book.id}, Author: {book.author} <br/>'
+    return HttpResponse(response_content)
