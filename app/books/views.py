9c4034781df0caf654a9834f9db069d03d6ad670
AndreyKhoroshiy <Khoroshiy256@gmail.com>
2021-02-14 02:17:16 +0200

    add function author_list

--- app/books/views.py
+++ app/books/views.py
@@ -1,9 +1,17 @@
 from django.http import HttpResponse
 from books.models import Book
+from books.models import Author
 
 
 def book_list(request):
     response_content = ''
     for book in Book.objects.all():
         response_content += f'ID: {book.id}, Author: {book.author} <br/>'
+    return HttpResponse(response_content)
+
+
+def author_list(request):
+    response_content = ''
+    for author in Author.objects.all():
+        response_content += f'ID: {author.id}, Author: {author.last_name} <br/>'
     return HttpResponse(response_content)
