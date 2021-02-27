fbbe1c21d8b451ac1e82fc47a1f59a5c58a6c3ec
AndreyKhoroshiy <Khoroshiy256@gmail.com>
2021-02-28 00:09:31 +0200

    add AuthorForm

--- app/books/forms.py
+++ app/books/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
 from books.models import Book
+from books.models import Author
 
 
 class BookForm(forms.ModelForm):
@@ -12,4 +13,18 @@ class BookForm(forms.ModelForm):
             'publish_year',
             'review',
             'condition',
+        )
+
+
+class AuthorForm(forms.ModelForm):
+    class Meta:
+        model = Author
+        fields = (
+            'first_name',
+            'last_name',
+            'date_of_birth',
+            'date_of_death',
+            'country',
+            'gender',
+            'native_language',
         )
