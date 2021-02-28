1fda1cbcf8431cad1436e0286668701c4113bf8d
AndreyKhoroshiy <Khoroshiy256@gmail.com>
2021-03-01 01:49:11 +0200

    add name path

--- app/booksharing/urls.py
+++ app/booksharing/urls.py
@@ -6,22 +6,14 @@ from books import views
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('', views.index),
-
-    path('books/list/', views.book_list),
-
-    path('authors/list/', views.author_list),
-
-    path('books/create/', views.book_create),
-
-    path('authors/create/', views.author_create),
-
-    path('books/update/<int:pk>/', views.book_update),
-
-    path('authors/update/<int:pk>/', views.author_update),
-
-    path('books/delete/<int:pk>/', views.book_delete),
-
-    path('authors/delete/<int:pk>/', views.author_delete),
+    path('', views.index, name='index'),
+    path('books/list/', views.book_list, name='books-list'),
+    path('authors/list/', views.author_list, name='authors-list'),
+    path('books/create/', views.book_create, name='books-create'),
+    path('authors/create/', views.author_create, name='authors-create'),
+    path('books/update/<int:pk>/', views.book_update, name='books-update'),
+    path('authors/update/<int:pk>/', views.author_update, name='authors-update'),
+    path('books/delete/<int:pk>/', views.book_delete, name='books-delete'),
+    path('authors/delete/<int:pk>/', views.author_delete, name='authors-delete'),
 
 ]
