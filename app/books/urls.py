add3c598a1b599700865ce57209e6d2844e8bcb9
AndreyKhoroshiy <Khoroshiy256@gmail.com>
2021-03-14 15:17:19 +0200

    add url for MyBooksList

--- app/books/urls.py
+++ app/books/urls.py
@@ -6,6 +6,7 @@ app_name = 'books'
 urlpatterns = [
 
     path('books-list/', views.BookList.as_view(), name='list'),
+    path('books-list/my-books/', views.MyBooksList.as_view(), name='my-books'),
     path('author-list/', views.AuthorList.as_view(), name='authors-list'),
     path('books-create/', views.BookCreate.as_view(), name='create'),
     path('authors-create/', views.AuthorCreate.as_view(), name='authors-create'),
