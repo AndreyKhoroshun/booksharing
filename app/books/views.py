b9412ea5e3d26cac62a4d1e9afef2225756c6cb0
AndreyKhoroshiy <Khoroshiy256@gmail.com>
2021-03-14 15:14:40 +0200

    add class MyBooksList

--- app/books/views.py
+++ app/books/views.py
@@ -5,6 +5,14 @@ from django.views.generic import (
     CreateView, UpdateView, DeleteView, ListView, TemplateView)
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
+from books.forms import BookForm, AuthorForm
+
+
+class FormUserKwargMixin:
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
 
 
 class Index(TemplateView):
@@ -15,62 +23,41 @@ class BookList(ListView):
     queryset = Book.objects.all().select_related('author', 'category')
 
 
+class MyBooksList(LoginRequiredMixin, ListView):
+    queryset = Book.objects.all().select_related('author')
+    template_name = 'books/my_books.html'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(user=self.request.user)
+
+
 class AuthorList(ListView):
     queryset = Author.objects.all()
 
 
-class BookCreate(LoginRequiredMixin, CreateView):
+class BookCreate(FormUserKwargMixin, CreateView):
     model = Book
-    success_url = reverse_lazy('books:list')
-    fields = (
-        'author',
-        'title',
-        'category',
-        'publish_year',
-        'review',
-        'condition',
-    )
+    success_url = reverse_lazy('books:my-books')
+    form_class = BookForm
 
 
-class AuthorCreate(LoginRequiredMixin, CreateView):
+class AuthorCreate(FormUserKwargMixin, CreateView):
     model = Author
-    success_url = reverse_lazy('authors-list')
-    fields = (
-        'first_name',
-        'last_name',
-        'date_of_birth',
-        'date_of_death',
-        'country',
-        'gender',
-        'native_language',
-    )
+    success_url = reverse_lazy('books:authors-list')
+    form_class = AuthorForm
 
 
-class BookUpdate(LoginRequiredMixin, UpdateView):
+class BookUpdate(FormUserKwargMixin, UpdateView):
     model = Book
-    success_url = reverse_lazy('books:list')
-    fields = (
-        'author',
-        'title',
-        'category',
-        'publish_year',
-        'review',
-        'condition',
-    )
+    success_url = reverse_lazy('books:my-books')
+    form_class = BookForm
 
 
-class AuthorUpdate(LoginRequiredMixin, UpdateView):
+class AuthorUpdate(FormUserKwargMixin, UpdateView):
     model = Author
-    success_url = reverse_lazy('authors-list')
-    fields = (
-        'first_name',
-        'last_name',
-        'date_of_birth',
-        'date_of_death',
-        'country',
-        'gender',
-        'native_language',
-    )
+    success_url = reverse_lazy('books:authors-list')
+    form_class = AuthorForm
 
 
 class BookDelete(LoginRequiredMixin, DeleteView):
