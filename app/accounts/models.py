682a55aec8ef01635c31a8d29e8193d433946403
AndreyKhoroshiy <Khoroshiy256@gmail.com>
2021-03-20 01:52:38 +0200

    add class ContactUs

--- app/accounts/models.py
+++ app/accounts/models.py
@@ -1,5 +1,13 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
     pass
+
+
+class ContactUs(models.Model):
+    full_name = models.CharField(max_length=128)
+    contact_to_email = models.EmailField()
+    message = models.CharField(max_length=1024)
+    created = models.DateField(auto_now_add=True)
