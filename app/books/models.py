bf5e6bd991dee2ff6be0ef8d11aee2b89e3e2671
AndreyKhoroshiy <Khoroshiy256@gmail.com>
2021-03-22 23:08:28 +0200

    add class RequestBook

--- app/books/models.py
+++ app/books/models.py
@@ -39,3 +39,29 @@ class Log(models.Model):
     path = models.CharField(max_length=512)
     method = models.CharField(max_length=128)
     time = models.PositiveIntegerField()
+
+
+class RequestBook(models.Model):
+
+    STATUS_IN_PROGRESS = 10
+    STATUS_CONFIRMED = 20
+    STATUS_REJECT = 30
+    STATUS_SENT_TO_RECIPIENT = 40
+    STATUS_RECIPIENT_RECEIVED_BOOK = 50
+    STATUS_SENT_BACK_TO_OWNER = 60
+    STATUS_OWNER_RECEIVED_BACK = 70
+
+    REQUEST_STATUSES = (
+        (STATUS_IN_PROGRESS, 'In Progress'),
+        (STATUS_CONFIRMED, 'Confirmed'),
+        (STATUS_REJECT, 'Reject'),
+        (STATUS_SENT_TO_RECIPIENT, 'Sent via mail Service'),
+        (STATUS_RECIPIENT_RECEIVED_BOOK, 'received Book'),
+        (STATUS_SENT_BACK_TO_OWNER, 'Sent Back'),
+        (STATUS_OWNER_RECEIVED_BACK, 'Received Back (Final)'),
+    )
+
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    recipient = models.ForeignKey('accounts.User', on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    status = models.PositiveSmallIntegerField(choices=REQUEST_STATUSES)
