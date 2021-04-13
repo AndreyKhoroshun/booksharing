049d236025308bb132b79763c2c6c78a0191e7db
AndreyKhoroshiy <Khoroshiy256@gmail.com>
2021-04-14 02:52:22 +0300

    add static_root

--- app/booksharing/settings.py
+++ app/booksharing/settings.py
@@ -139,6 +139,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+STATIC_ROOT = BASE_DIR / '..' / 'static_content' / 'static'
+
 MEDIA_ROOT = BASE_DIR / '..' / 'media'
 MEDIA_URL = '/media/'
 
